2c98cda8a27a0ff3a777c6fe4c8e64dc339c4180
Rishav Sharma <rishavtosharma@gmail.com>
2024-04-17 17:27:39 +0530

    ci: shifted map code to separate component, initialised lodash and react-icons

--- client/src/components/mapComponent/index.tsx
+++ client/src/components/mapComponent/index.tsx
@@ -1,32 +1,39 @@
-import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import Search from "../search";
 
-const MapComponent = () => {
-    const mapContainer = useRef<HTMLDivElement | null>(null);
+type MapComponentProps = {
+    map: React.MutableRefObject<mapboxgl.Map | null>;
+    mapContainer: React.MutableRefObject<HTMLDivElement | null>;
+    lng: number;
+    lat: number;
+    zoom: number;
+    setLng: (lng: number) => void;
+    setLat: (lat: number) => void;
+    setZoom: (zoom: number) => void;
+};
 
-    useEffect(() => {
-        if (mapContainer.current) {
-            map.current = new mapboxgl.Map({
-                container: mapContainer.current,
-                style: "mapbox://styles/mapbox/streets-v11",
-                center: [lng.current, lat.current],
-                zoom: zoom.current,
-            });
-
-            map.current.on("move", () => {
-                const center = map.current?.getCenter();
-                const zoomLevel = map.current?.getZoom();
-                lng.current = parseFloat(center?.lng.toFixed(4) || "0");
-                lat.current = parseFloat(center?.lat.toFixed(4) || "0");
-                zoom.current = parseFloat(zoomLevel?.toFixed(2) || "0");
-            });
-        }
-
-        // testing the cleanup function
-        setRenderCount((prevRenderCount) => prevRenderCount + 1);
-
-        return () => map.current?.remove();
-    }, []);
-
-    return <div className='h-[600px] w-[1284px]' ref={mapContainer}></div>;
+const MapComponent = ({
+    lng,
+    lat,
+    zoom,
+    setLng,
+    setLat,
+    setZoom,
+    mapContainer,
+}: MapComponentProps) => {
+    return (
+        <div className='h-[600px] w-[1284px]' ref={mapContainer}>
+            <div className='relative'>
+                <div className='absolute top-0 left-0 flex items-center justify-between px-2 py-3 m-3 w-[calc(100%_-_24px)] rounded-sm min-w-[560px]'>
+                    <div className='whitespace-nowrap flex items-center justify-center h-12 gap-2 px-4 py-2 font-mono bg-gray-700 rounded'>
+                        <span className='flex-1'>Longitude: {lng} | </span>
+                        <span className='flex-1'> Latitude: {lat} | </span>
+                        <span className='flex-1'> Zoom: {zoom}</span>
+                    </div>
+                    <Search setLng={setLng} setLat={setLat} setZoom={setZoom} />
+                </div>
+            </div>
+        </div>
+    );
 };
 export default MapComponent;
--- client/src/components/search/index.tsx
+++ client/src/components/search/index.tsx
@@ -1,37 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { debounce } from "lodash";
 
-const Search = () => {
-    const [searchTerm, setSearchTerm] = useState("");
+// React Icons
+import { IoMdSearch } from "react-icons/io";
 
-    const handleSearch = async (event) => {
-        event.preventDefault();
+interface SearchResult {
+    name: string;
+    place_formatted: string;
+}
 
+const Search = ({ setLng, setLat, setZoom }) => {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+    // const [sessionToken, setSessionToken] = useState(""); // Add this line
+
+    const debouncedSearch = debounce(async (searchTerm: string) => {
         const mapboxToken = import.meta.env
             .VITE_APP_MAPBOX_ACCESS_TOKEN_WITH_STATIC_IMAGE;
+
+        const sessionToken = Math.random().toString(36).substr(2, 15); // Add this line
         const res = await fetch(
-            `https://api.mapbox.com/search/searchbox/v1/suggest?limit=5&language=en&q=${searchTerm}&access_token=${mapboxToken}`
+            `https://api.mapbox.com/search/searchbox/v1/suggest?limit=5&language=en&q=${searchTerm}&access_token=${mapboxToken}&session_token=${sessionToken}`
         );
         const data = await res.json();
+        setSearchResults(data.suggestions);
         console.log(data);
+    }, 50); // Adjust the delay as needed
 
-        console.log(searchTerm);
-    };
+    console.log("searchResults", searchResults);
+
+    useEffect(() => {
+        if (searchTerm) {
+            debouncedSearch(searchTerm);
+        }
+    }, [searchTerm]);
 
     return (
-        <div className='flex items-center justify-center mt-5'>
-            <input
-                type='text'
-                placeholder='Search...'
-                className='p-2 mr-3 border border-gray-400 rounded'
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <button
-                onClick={handleSearch}
-                className='p-2 text-white bg-blue-500 rounded'
-            >
-                Search
-            </button>
+        <div className='relative flex flex-col items-center justify-center'>
+            <div className='flex items-center p-2 bg-white border-gray-400 rounded'>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    className=' text-black bg-transparent outline-none'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            e.preventDefault(); // Prevent form submission
+
+                            // Add logic here to set the map's center and zoom
+                            // based on the selected search result
+                            console.log(searchResults[0]);
+                            // setLng(searchResults[0].bbox[0]);
+                            // setLat(searchResults[0].bbox[1]);
+                            // setZoom(12);
+                        }
+                    }}
+                />
+                <IoMdSearch className=' text-xl text-gray-400' />
+            </div>
+
+            {
+                <div className='bg-white/90 top-12 absolute left-0 right-0 flex flex-col max-w-full gap-1 p-3 rounded'>
+                    {searchResults.map((result, i) => (
+                        <div key={i} className='flex flex-col text-black'>
+                            <span className='text-semibold'>{result.name}</span>
+                            <span className='-mt-1 text-sm text-gray-500'>
+                                {result.place_formatted}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
     );
 };

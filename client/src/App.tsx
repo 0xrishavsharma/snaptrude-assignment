906e87b270770d7a64d619da411c0eccb1c1bd0e
Rishav Sharma <rishavtosharma@gmail.com>
2024-04-17 17:27:39 +0530

    ci: shifted map code to separate component, initialised lodash and react-icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import mapboxgl from "mapbox-gl";
 import { useRef, useEffect, useState } from "react";
 import SceneComponent from "./components/sceneComponent";
 import {
@@ -14,93 +13,49 @@ import {
 } from "@babylonjs/core";
 import MapComponent from "./components/mapComponent";
 import Navbar from "./components/navbar";
-import Search from "./components/search";
+import mapboxgl from "mapbox-gl";
 
 function App() {
-    const mapContainer = useRef<HTMLDivElement | null>(null);
-    const map = useRef<mapboxgl.Map | null>(null);
-    // const [lng, setLng] = useState(-70.9);
-    // const [lat, setLat] = useState(42.35);
-    // const [zoom, setZoom] = useState(9);
-    const lng = useRef(-70.9);
-    const lat = useRef(42.35);
-    const zoom = useRef(9);
-    const [renderCount, setRenderCount] = useState(0);
+    const accessToken = import.meta.env.VITE_APP_MAPBOX_ACCESS_TOKEN;
+    const mapStyle = "mapbox://styles/mapbox/streets-v11";
 
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
     const [mapWidth, setMapWidth] = useState(300);
     const [mapHeight, setMapHeight] = useState(200);
-
     const [mapImage, setMapImage] = useState<string | null>(null);
 
-    mapboxgl.accessToken = import.meta.env
-        .VITE_APP_MAPBOX_ACCESS_TOKEN as string;
-
-    useEffect(() => {
-        if (mapContainer.current) {
-            map.current = new mapboxgl.Map({
-                container: mapContainer.current,
-                style: "mapbox://styles/mapbox/streets-v11",
-                center: [lng.current, lat.current],
-                zoom: zoom.current,
-            });
-
-            map.current.on("move", () => {
-                const center = map.current?.getCenter();
-                const zoomLevel = map.current?.getZoom();
-                lng.current = parseFloat(center?.lng.toFixed(4) || "0");
-                lat.current = parseFloat(center?.lat.toFixed(4) || "0");
-                zoom.current = parseFloat(zoomLevel?.toFixed(2) || "0");
-            });
-        }
-
-        // testing the cleanup function
-        setRenderCount((prevRenderCount) => prevRenderCount + 1);
-
-        return () => map.current?.remove();
-    }, []);
-
-    const captureMap = async () => {
-        const res = await fetch(
-            `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${
-                lng.current
-            },${lat.current},${
-                zoom.current
-            },0/${mapWidth}x${mapHeight}?access_token=${
-                import.meta.env.VITE_APP_MAPBOX_ACCESS_TOKEN_WITH_STATIC_IMAGE
-            }`
-        );
-        const blob = await res.blob();
-        setMapImage(URL.createObjectURL(blob));
-    };
+    const mapContainer = useRef<HTMLDivElement | null>(null);
+    const map = useRef<mapboxgl.Map | null>(null);
 
     let box: Mesh;
 
     const onSceneReady = (scene: Scene) => {
-        // This creates and positions a free camera (non-mesh)
+        // Creates and positions a free camera (non-mesh)
         const camera = new FreeCamera("camera1", new Vector3(5, 5, -10), scene);
 
-        // This targets the camera to scene origin
+        // Targets the camera to scene origin
         camera.setTarget(Vector3.Zero());
 
         const canvas = scene.getEngine().getRenderingCanvas();
 
-        // This attaches the camera to the canvas
+        // Attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        // Creates a light, aiming 0,1,0 - to the sky (non-mesh)
         const light = new HemisphericLight(
             "light",
             new Vector3(0, 1, 0),
             scene
         );
 
-        // Default intensity is 1. Let's dim the light a small amount
+        // Default intensity is 1. Dimming the light by a small amount
         light.intensity = 0.8;
 
-        // Our built-in 'box' shape.
         box = MeshBuilder.CreateBox("box", { size: 5 }, scene);
 
-        // Move the box upward 1/2 its height
+        // Moves the box upward 1/2 its height
         box.position.y = 1;
 
         const material = new StandardMaterial("material", scene);
@@ -120,20 +75,53 @@ function App() {
         }
     };
 
+    const captureMap = async () => {
+        const res = await fetch(
+            `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${lng},${lat},${zoom},0/${mapWidth}x${mapHeight}?access_token=${
+                import.meta.env.VITE_APP_MAPBOX_ACCESS_TOKEN_WITH_STATIC_IMAGE
+            }`
+        );
+        const blob = await res.blob();
+        setMapImage(URL.createObjectURL(blob));
+    };
+
+    mapboxgl.accessToken = accessToken;
+
+    useEffect(() => {
+        if (mapContainer.current) {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: mapStyle,
+                center: [lng, lat],
+                zoom: zoom,
+            });
+
+            map.current.on("move", () => {
+                const center = map.current?.getCenter();
+                const zoomLevel = map.current?.getZoom();
+
+                setLng(parseFloat(center?.lng.toFixed(4) || "0"));
+                setLat(parseFloat(center?.lat.toFixed(4) || "0"));
+                setZoom(parseFloat(zoomLevel?.toFixed(2) || "0"));
+            });
+        }
+
+        return () => map.current?.remove();
+    }, []);
+
     return (
         <div className='flex flex-col items-center justify-center gap-12'>
             <Navbar />
-            <Search />
-            <div className='h-[600px] w-[1284px]' ref={mapContainer}>
-                <div className='relative'>
-                    <div className='absolute top-0 left-0 px-2 py-3 m-3 font-mono text-white rounded-sm'></div>
-                    <div className=' top-2 left-2 absolute flex items-center h-12 px-4 py-2 bg-gray-700'>
-                        Longitude: {lng.current} | Latitude: {lat.current} |
-                        Zoom: {zoom.current}
-                    </div>
-                </div>
-            </div>
-            {/* <MapComponent /> */}
+            <MapComponent
+                map={map}
+                mapContainer={mapContainer}
+                lng={lng}
+                lat={lat}
+                zoom={zoom}
+                setLng={setLng}
+                setLat={setLat}
+                setZoom={setZoom}
+            />
             <button onClick={captureMap}> Capture Map as an Image</button>
             <SceneComponent
                 antialias={true}
